83a84ca407d7a441306cab753b44c52fbbf354c1
David <david.pintar00@gmail.com>
2025-03-16 22:55:22 -0400

    adding appsettings.Example.json and DbConnection

--- LibraryAPI/Program.cs
+++ LibraryAPI/Program.cs
@@ -1,7 +1,13 @@
+using LibraryAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Connect to database
+builder.Services.AddDbContext<LibraryDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.")));
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
